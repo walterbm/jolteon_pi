01c9b8cc6f988d901531aeed56ade77f0482ea91
Walter Beller-Morales <walter.beller.morales@gmail.com>
2016-07-14 01:06:21 -0400

    add test relay endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const Gpio = require('onoff').Gpio;
 
 const led = new Gpio(16, 'out');
+const relay = new Gpio(17, 'out');
 
 app.get('/', function (req, res) {
   res.send('Welcome to the Raspberry Pi API');
@@ -11,6 +12,11 @@ app.get('/', function (req, res) {
 app.get('/led', function (req, res) {
   led.writeSync(led.readSync() === 0 ? 1 : 0);
   res.send(`led status ${led.readSync()}`);
+});
+
+app.get('/relay', function (req, res) {
+  relay.writeSync(relay.readSync() === 0 ? 1 : 0);
+  res.send(`relay status ${relay.readSync()}`);
 });
 
 app.listen(3000, function () {
